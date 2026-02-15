5c8f81e4dab12e6a907835db1ce5bb6166cd29da
Balázs Nádasdi <nadasdibalazs@gmail.com>
2026-02-15 23:35:30 +0100

    Kartrace - initial commit Dummy Spring Boot application Amend: fixed redundant public main

---
+++ src/main/java/com/mylaps/kartrace/KartraceApplication.java
@@ -0,0 +1,12 @@
+package com.mylaps.kartrace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KartraceApplication {
+	static void main(String[] args) {
+		SpringApplication.run(KartraceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mylaps/kartrace/KartraceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mylaps.kartrace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KartraceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
